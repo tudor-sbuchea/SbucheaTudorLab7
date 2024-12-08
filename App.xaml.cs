df61ece77ea22adbf9bb29e66abc0960913ca317
TUDOR-PETRU SBUCHEA <tudor.sbuchea@stud.ubbcluj.ro>
2024-12-08 16:25:22 +0200

    Laborator8

--- App.xaml.cs
+++ App.xaml.cs
@@ -1,7 +1,26 @@
-﻿namespace SbucheaTudorLab7
+﻿using System;
+using SbucheaTudorLab7.Data;
+using System.IO;
+
+
+namespace SbucheaTudorLab7
 {
     public partial class App : Application
     {
+        static ShoppingListDatabase database;
+        public static ShoppingListDatabase Database
+        {
+            get
+            {
+                if (database == null)
+                {
+                    database = new
+                   ShoppingListDatabase(Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.
+                   LocalApplicationData), "ShoppingList.db3"));
+                }
+                return database;
+            }
+        }
         public App()
         {
             InitializeComponent();
